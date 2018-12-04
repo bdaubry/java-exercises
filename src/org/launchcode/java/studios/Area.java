e12134f10b62d71503a2735e1ea13e2d103a0bad
Brian Aubry <bdaubry@gmail.com>
2018-12-03 18:39:18 -0600

    Area studio attempt before class

---
+++ src/org/launchcode/java/studios/Area.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.studios;
+
+import java.util.Scanner;
+
+public class Area {
+
+
+
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        double radius;
+        double area;
+        Scanner input;
+
+        input = new Scanner(System.in);
+
+        System.out.println("What is the radius of the circle?");
+        radius = input.nextDouble();
+
+        while (radius < 0) {
+            System.out.println("Radius can't be negative");
+            System.out.println("What is the radius of the circle?");
+            radius = input.nextDouble();
+        }
+
+        area = pi * radius * radius;
+
+        System.out.println("The area of a circle with radius " + radius + " is " + area);
+
+    }
+
+}
